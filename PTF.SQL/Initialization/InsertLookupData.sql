7370729b60504203bc071aed08090c871e04e3d8
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 17:23:21 +0000

    (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1791. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/PTF/TRUNK/PTF;C1247

---
+++ PTF.SQL/Initialization/InsertLookupData.sql
@@ -0,0 +1,267 @@
+﻿-- Note Types
+insert into NoteTypes (NoteType)
+values ('Induction')
+
+insert into NoteTypes (NoteType)
+values ('Elongation/Germination')
+
+insert into NoteTypes (NoteType)
+values ('Recallusing')
+
+insert into NoteTypes (NoteType)
+values ('Rooting')
+
+-- Status
+
+Insert Into [Status] ([Status])
+values ('Initiated')
+
+Insert Into [Status] ([Status])
+values ('Pending')
+
+Insert Into [Status] ([Status])
+values ('Complete')
+
+Insert Into [Status] ([Status])
+values ('Hold-Pending Contract')
+
+Insert Into [Status] ([Status])
+values ('Cancelled')
+
+-- Agro Strain
+
+insert into AgroStrains (AgroStrain)
+values ('LBA4404')
+
+insert into AgroStrains (AgroStrain)
+values ('C58')
+
+insert into AgroStrains (AgroStrain)
+values ('ASZ')
+
+insert into AgroStrains (AgroStrain)
+values ('GV3103')
+
+insert into AgroStrains (AgroStrain)
+values ('EHA101')
+
+insert into AgroStrains (AgroStrain)
+values ('EHA105')
+
+insert into AgroStrains (AgroStrain)
+values ('ASE')
+
+insert into AgroStrains (AgroStrain)
+values ('ABI')
+
+-- Plant Selection
+
+insert into PlantSelections (PlantSelection)
+values ('35s nptii')
+
+insert into PlantSelections (PlantSelection)
+values ('nos nptii')
+
+insert into PlantSelections (PlantSelection)
+values ('mas nptii')
+
+insert into PlantSelections (PlantSelection)
+values ('35s hygro')
+
+insert into PlantSelections (PlantSelection)
+values ('35s bar')
+
+insert into PlantSelections (PlantSelection)
+values ('glyphosate')
+
+insert into PlantSelections (PlantSelection)
+values ('glufosinate')
+
+insert into PlantSelections (PlantSelection)
+values ('mannose')
+
+insert into PlantSelections (PlantSelection)
+values ('hygromycin')
+
+insert into PlantSelections (PlantSelection)
+values ('kanamycin')
+
+-- Trans Genes
+
+insert into TransGenes (TransGene)
+values ('Agronomic Properties')
+
+insert into TransGenes (TransGene)
+values ('Fungus Resistant')
+
+insert into TransGenes (TransGene)
+values ('Herbicide Tolerant')
+
+insert into TransGenes (TransGene)
+values ('Insect Resistant')
+
+insert into TransGenes (TransGene)
+values ('Marker Genes')
+
+insert into TransGenes (TransGene)
+values ('Nematode Resistant')
+
+insert into TransGenes (TransGene)
+values ('Product Quality')
+
+insert into TransGenes (TransGene)
+values ('Virus Resistant')
+
+insert into TransGenes (TransGene)
+values ('Other')
+
+-- Crops
+
+insert into Crops (Crop)
+values ('Alfalfa')
+
+insert into Crops (Crop)
+values ('Apple')
+
+insert into Crops (Crop)
+values ('Broccoli')
+
+insert into Crops (Crop)
+values ('Canola')
+
+insert into Crops (Crop)
+values ('Carrot')
+
+insert into Crops (Crop)
+values ('Cauliflower')
+
+insert into Crops (Crop)
+values ('Cucumber')
+
+insert into Crops (Crop)
+values ('Citrus')
+
+insert into Crops (Crop)
+values ('Grape')
+
+insert into Crops (Crop)
+values ('Lettuce')
+
+insert into Crops (Crop)
+values ('Melon')
+
+insert into Crops (Crop)
+values ('M.truncatula')
+
+insert into Crops (Crop)
+values ('N.tabacum SR1')
+
+insert into Crops (Crop)
+values ('N.tabacum xanthi')
+
+insert into Crops (Crop)
+values ('N benthamiana')
+
+insert into Crops (Crop)
+values ('Squash')
+
+insert into Crops (Crop)
+values ('Rice')
+
+insert into Crops (Crop)
+values ('Tobacco')
+
+insert into Crops (Crop)
+values ('Tomato')
+
+insert into Crops (Crop)
+values ('Walnut')
+
+
+-- Cultivars
+
+insert into Cultivars (Cultivar)
+values ('Moneymaker')
+
+insert into Cultivars (Cultivar)
+values ('T5')
+
+insert into Cultivars (Cultivar)
+values ('UC82B')
+
+insert into Cultivars (Cultivar)
+values ('MicroTom')
+
+insert into Cultivars (Cultivar)
+values ('benthi')
+
+insert into Cultivars (Cultivar)
+values ('Eureka')
+
+insert into Cultivars (Cultivar)
+values ('Kitaake')
+
+insert into Cultivars (Cultivar)
+values ('2525-14')
+
+insert into Cultivars (Cultivar)
+values ('Diana')
+
+insert into Cultivars (Cultivar)
+values ('Marisaka')
+
+insert into Cultivars (Cultivar)
+values ('Xanthi')
+
+insert into Cultivars (Cultivar)
+values ('SR-1')
+
+insert into Cultivars (Cultivar)
+values ('Carrizo')
+
+-- Genotypes
+
+insert into Genotypes (Genotype)
+values ('MoneyMaker')
+
+insert into Genotypes (Genotype)
+values ('T5')
+
+insert into Genotypes (Genotype)
+values ('UC82B')
+
+insert into Genotypes (Genotype)
+values ('MicroTom')
+
+insert into Genotypes (Genotype)
+values ('2525-14')
+
+insert into Genotypes (Genotype)
+values ('regenS')
+
+insert into Genotypes (Genotype)
+values ('regensy')
+
+insert into Genotypes (Genotype)
+values ('Alisa Craig')
+
+insert into Genotypes (Genotype)
+values ('SR1')
+
+insert into Genotypes (Genotype)
+values ('Xanthi')
+
+insert into Genotypes (Genotype)
+values ('Benthi')
+
+insert into Genotypes (Genotype)
+values ('Diana')
+
+insert into Genotypes (Genotype)
+values ('Mariska')
+
+insert into Genotypes (Genotype)
+values ('Kitaake')
+
+insert into Genotypes (Genotype)
+values ('Carrizzo')
