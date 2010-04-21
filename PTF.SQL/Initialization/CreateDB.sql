cdbf2b2a2f74b8f14c89fd8fc94cea85177df38e
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 17:27:55 +0000

    added a piece that adds a trigger to the experiment table. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 2222. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/PTF/TRUNK/PTF;C1403

--- PTF.SQL/Initialization/CreateDB.sql
+++ PTF.SQL/Initialization/CreateDB.sql
@@ -5069,3 +5069,64 @@ REFERENCES [dbo].[PlantSelections] ([PlantSelectionID])
 GO
 ALTER TABLE [dbo].[SubOrders] CHECK CONSTRAINT [FK_SubOrders_SelectableMarkers]
 GO
+
+
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Trigger (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- See additional Create Trigger templates for more
+-- examples of different Trigger statements.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER [dbo].[GenerateExperimentCodeInfo]
+   ON  [dbo].[Experiments]
+   AFTER INSERT
+AS 
+BEGIN
+
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    declare @id int, @start datetime, @operator int
+	declare @yearcode int, @experimentnum int
+
+	select @id = eid, @start = datestarted, @operator = operatorid from inserted
+
+	-- get the year code
+	declare @year varchar(4)
+	select @year = datepart(yy, @start)
+	set @year = substring(@year, 3, 2)
+	set @yearCode = cast(@year as int)
+
+	-- get the operator experiment number
+	select @experimentnum = max(operatorexperimentnum) from Experiments where effectiveoperatorid = dbo.udf_geteffectiveid(@operator)
+
+	if (@experimentnum is null)
+		set @experimentnum = 1
+	else
+		SET @experimentnum = @experimentnum + 1
+
+	-- update the row
+	update Experiments
+	set yearcode = @yearcode, operatorexperimentnum = @experimentnum
+	where eid = @id
+
+END
+GO
