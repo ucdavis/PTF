5181688805e867a2911a0a862ba92530def5033a
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 17:25:07 +0000

    fixed up the order object. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1918. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/PTF/TRUNK/PTF;C1297

--- PTF.Core/Domain/Order.cs
+++ PTF.Core/Domain/Order.cs
@@ -18,69 +18,69 @@ namespace CAESDO.PTF.Core.Domain
         public virtual string Contact { get; set; }
         [IgnoreNulls]
         [StringLengthValidator(20)]
-        //[RegexValidator("([\(]?(?<AreaCode>[0-9]{3})[\)]?)?[ \.\-]?(?<Exchange>[0-9]{3})[ \.\-](?<Number>[0-9]{4})")]
         public virtual string ContactPhone { get; set; }
         [StringLengthValidator(50)]
         [NotNullValidator]
-        //[RegexValidator("^((([a-z]|[0-9]|!|#|$|%|&|'|\*|\+|\-|/|=|\?|\^|_|`|\{|\||\}|~)+(\.([a-z]|[0-9]|!|#|$|%|&|'|\*|\+|\-|/|=|\?|\^|_|`|\{|\||\}|~)+)*)@((((([a-z]|[0-9])([a-z]|[0-9]|\-){0,61}([a-z]|[0-9])\.))*([a-z]|[0-9])([a-z]|[0-9]|\-){0,61}([a-z]|[0-9])\.(af|ax|al|dz|as|ad|ao|ai|aq|ag|ar|am|aw|au|at|az|bs|bh|bd|bb|by|be|bz|bj|bm|bt|bo|ba|bw|bv|br|io|bn|bg|bf|bi|kh|cm|ca|cv|ky|cf|td|cl|cn|cx|cc|co|km|cg|cd|ck|cr|ci|hr|cu|cy|cz|dk|dj|dm|do|ec|eg|sv|gq|er|ee|et|fk|fo|fj|fi|fr|gf|pf|tf|ga|gm|ge|de|gh|gi|gr|gl|gd|gp|gu|gt| gg|gn|gw|gy|ht|hm|va|hn|hk|hu|is|in|id|ir|iq|ie|im|il|it|jm|jp|je|jo|kz|ke|ki|kp|kr|kw|kg|la|lv|lb|ls|lr|ly|li|lt|lu|mo|mk|mg|mw|my|mv|ml|mt|mh|mq|mr|mu|yt|mx|fm|md|mc|mn|ms|ma|mz|mm|na|nr|np|nl|an|nc|nz|ni|ne|ng|nu|nf|mp|no|om|pk|pw|ps|pa|pg|py|pe|ph|pn|pl|pt|pr|qa|re|ro|ru|rw|sh|kn|lc|pm|vc|ws|sm|st|sa|sn|cs|sc|sl|sg|sk|si|sb|so|za|gs|es|lk|sd|sr|sj|sz|se|ch|sy|tw|tj|tz|th|tl|tg|tk|to|tt|tn|tr|tm|tc|tv|ug|ua|ae|gb|us|um|uy|uz|vu|ve|vn|vg|vi|wf|eh|ye|zm|zw|com|edu|gov|int|mil|net|org|biz|info|name|pro|aero|coop|museum|arpa))|(((([0-9]){1,3}\.){3}([0-9]){1,3}))|(\[((([0-9]){1,3}\.){3}([0-9]){1,3})\])))$")]
         public virtual string ContactEmail { get; set; }
 
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(100)]
         public virtual string MailingAddress1 { get; set; }
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(100)]
         public virtual string MailingAddress2 { get; set; }
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(50)]
         public virtual string MailingCity { get; set; }
         [IgnoreNulls]
         [StringLengthValidator(50)]
         public virtual string MailingInternationalState { get; set; }
         public virtual State MailingState { get; set; }
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(12)]
         public virtual string MailingZip { get; set; }
+        [NotNullValidator]
         public virtual Country MailingCountry { get; set; }
 
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(100)]
         public virtual string ShippingAddress1 { get; set; }
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(100)]
         public virtual string ShippingAddress2 { get; set; }
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(50)]
         public virtual string ShippingCity { get; set; }
         [IgnoreNulls]
         [StringLengthValidator(50)]
         public virtual string ShippingInternationalState { get; set; }
         public virtual State ShippingState { get; set; }
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(12)]
         public virtual string ShippingZip { get; set; }
+        [NotNullValidator]
         public virtual Country ShippingCountry { get; set; }
-        
-        
-        [IgnoreNulls]
-        [StringLengthValidator(50)]
-        public virtual string Cultivar { get; set; }
-        [IgnoreNulls]
+
+        [NotNullValidator]
         [StringLengthValidator(50)]
         public virtual string Plasmid { get; set; }
         [NotNullValidator]
         [StringLengthValidator(50)]
         public virtual string BacterialSelection { get; set; }
         [NotNullValidator]
-        [StringLengthValidator(50)]
         public virtual AgroStrain AgroStrain { get; set; }
-        [IgnoreNulls]
+        [NotNullValidator]
         [StringLengthValidator(50)]
-        public virtual string Construct { get; set; }
-        [IgnoreNulls]
+        public virtual string PIConstructName { get; set; }
+        [NotNullValidator]
         [StringLengthValidator(50)]
-        public virtual string NameOfYourConstruct { get; set; }
+        public virtual string PICode { get; set; }
         public virtual TransGene TransGene { get; set; }
+        [IgnoreNulls]
+        public virtual GenoType GenoType { get; set; }
+        [IgnoreNulls]
+        [StringLengthValidator(50)]
+        public virtual string Trait { get; set; }
 
         [NotNullValidator]
         [StringLengthValidator(50)]
@@ -88,26 +88,26 @@ namespace CAESDO.PTF.Core.Domain
         [NotNullValidator]
         [StringLengthValidator(50)]
         public virtual string SelectableMarkerGene { get; set; }
+
         [IgnoreNulls]
-        [StringLengthValidator(50)]
-        public virtual string ScorableMarkerTerminator { get; set; }
         public virtual string AdditionalInformation { get; set; }
-        [IgnoreNulls]
+        [NotNullValidator]
+        public virtual bool OtherUCResearch { get; set; }
         [NotNullValidator]
         public virtual Guid UserID { get; set; }
+        
         [IgnoreNulls]
         [StringLengthValidator(20)]
         public virtual string RechargeNumber { get; set; }
-
+        [IgnoreNulls]
         public virtual bool ContractExecuted { get; set; }
         //[RegexValidator("^\d{2}-\d{3}-\d{3}[nm]$")]
+        [IgnoreNulls]
         public virtual string ShippingPermit { get; set; }
-        [NotNullValidator]
-        public virtual bool OtherUCResearch { get; set; }
-
-        public virtual IList<Construct> Constructs { get; set; }
-
         public virtual string Comments { get; set; }
+        [IgnoreNulls]
+        public virtual DateTime? DateReceived { get; set; }
+
         [IgnoreNulls]
         [StringLengthValidator(10)]
         public virtual string ArchiveBox { get; set; }
@@ -120,34 +120,8 @@ namespace CAESDO.PTF.Core.Domain
         [IgnoreNulls]
         [StringLengthValidator(10)]
         public virtual string Location { get; set; }
-        [IgnoreNulls]
-        [StringLengthValidator(50)]
-        public virtual string Trait { get; set; }
-        public virtual DateTime? DateReceived { get; set; }
 
-        public virtual string Status { 
-            get {
-                return this.HasPendingConstruct() ? "Pending" : "Complete";
-            } 
-        }
-
-        private bool HasPendingConstruct()
-        {
-            if (Constructs.Count == 0)
-            {
-                return true;
-            }
-
-            foreach (Construct c in Constructs)
-            {
-                if (c.Status.Name != "Complete")
-                {
-                    return true;
-                }
-            }
-
-            // all of the constructs are marked complete
-            return false;
-        }
+        public virtual IList<Construct> Constructs { get; set; }
+        public virtual IList<SubOrder> SubOrders { get; set; }
     }
 }
