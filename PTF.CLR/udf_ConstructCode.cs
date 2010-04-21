88b81a61b34c810c36bfe61ee5362b2cb2e097c7
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 17:23:32 +0000

    Created a few udf's to help with the generated columsn used in the construct and experiment tables.  (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1801. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/PTF/TRUNK/PTF;C1251

---
+++ PTF.CLR/udf_ConstructCode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlString udf_ConstructCode(int yearCode, int sequenceNumber, int subSequenceNumber)
+    {
+        string constructCode = "AT";
+
+        if (yearCode < 10)
+        {
+            constructCode += "0" + yearCode.ToString();
+        }
+        else
+        {
+            constructCode += yearCode.ToString();
+        }
+
+        if (sequenceNumber < 10)
+        {
+            constructCode += "00" + sequenceNumber.ToString();
+        }
+        else if (sequenceNumber >= 10 && sequenceNumber < 100)
+        {
+            constructCode += "0" + sequenceNumber.ToString();
+        }
+        else
+        {
+            constructCode += sequenceNumber.ToString();
+        }
+
+        if (subSequenceNumber > 0)
+        {
+            constructCode += "-";
+
+            if (subSequenceNumber < 10)
+            {
+                constructCode += "0" + subSequenceNumber.ToString();
+            }
+            else
+            {
+                constructCode += subSequenceNumber.ToString();
+            }
+        }
+
+        return new SqlString(constructCode);
+    }
+};
+
---
+++ PTF.CLR/udf_ExperimentCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlString udf_ExperimentCode(int yearCode, int operatorCode, int operatorExperimentNum)
+    {
+        string experimentCode;
+
+        experimentCode = yearCode.ToString();
+        experimentCode += operatorCode.ToString();
+        if (operatorExperimentNum < 10)
+        {
+            experimentCode += "00" + operatorExperimentNum.ToString();
+        }
+        else if (operatorExperimentNum < 100)
+        {
+            experimentCode += "0" + operatorExperimentNum.ToString();
+        }
+        else
+        {
+            experimentCode += operatorExperimentNum.ToString();
+        }
+
+        return (SqlString)experimentCode;
+    }
+};
+
