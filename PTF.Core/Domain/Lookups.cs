de443aa7f4c2afc9869369ea94504f4cf1de6d28
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 17:23:45 +0000

    Began writing the mapping files. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1814. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/PTF/TRUNK/PTF;C1256

---
+++ PTF.Core/Domain/Lookups.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+namespace CAESDO.PTF.Core.Domain
+{
+    public class NoteType : DomainObject<NoteType, int>
+    {
+        public NoteType() { }
+
+        [NotNullValidator]
+        [StringLengthValidator(50)]
+        public virtual string Name { get; set; }
+    }
+
+    public class Crop : DomainObject<Crop, int>
+    {
+        public Crop() { }
+        
+        [NotNullValidator]
+        [StringLengthValidator(50)]
+        public virtual string Name { get; set; }
+    }
+
+    public class AgroStrain : DomainObject<AgroStrain, int>
+    {
+        public AgroStrain() { }
+
+        [NotNullValidator]
+        [StringLengthValidator(50)]
+        public virtual string Name { get; set; }
+    }
+
+    public class Status : DomainObject<Status, int>
+    {
+        public Status() { }
+
+        [NotNullValidator]
+        [StringLengthValidator(50)]
+        public virtual string Name { get; set; }
+    }
+
+    public class SelectableMarker : DomainObject<SelectableMarker, int>
+    {
+        public SelectableMarker() { }
+        
+        [NotNullValidator]
+        [StringLengthValidator(50)]
+        public virtual string Name { get; set; }
+    }
+
+    public class GenoType : DomainObject<GenoType, int>
+    {
+        public GenoType() { }
+
+        [NotNullValidator]
+        [StringLengthValidator(50)]
+        public virtual string Name { get; set; }
+    }
+
+    public class TransGene : DomainObject<TransGene, int>
+    {
+        public TransGene() { }
+
+        [NotNullValidator]
+        [StringLengthValidator(50)]
+        public virtual string Name { get; set; }
+    }
+}
