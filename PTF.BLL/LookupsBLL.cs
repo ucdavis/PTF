1a646c09f204bd7c87b5f50df40fb6febfdb2920
SU for Alan Lai <srkirkland@gmail.com>
2010-04-21 17:23:47 +0000

    wrote the bll classes for the lookups. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1821. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/PTF/TRUNK/PTF;C1259

---
+++ PTF.BLL/LookupsBLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CAESDO.PTF.Core.Domain;
+
+namespace CAESDO.PTF.BLL
+{
+    public class NoteTypeBLL : GenericBLL<NoteType, int> { }
+    public class CropBLL : GenericBLL<Crop, int> { }
+    public class AgroStrainBLL : GenericBLL<AgroStrain, int> { }
+    public class StatusBLL : GenericBLL<Status, int> { }
+    public class SelectableMarkerBLL : GenericBLL<SelectableMarker, int> { }
+    public class GenoTypeBLL : GenericBLL<GenoType, int> { }
+    public class TransGeneBLL : GenericBLL<TransGene, int> { }
+}
